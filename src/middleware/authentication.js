aabcc4258a3a8ef9925151248e552c7a505e801f
Vitor Conti <vitorconti18@gmail.com>
2022-04-03 01:13:31 -0300

    feat: added middleware to all routes

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const { verify } = require("jsonwebtoken");
+const { validateProfile } = require("../../src/services/profileTypes");
+const profileEnum = require("../../src/enum/profileTypes");
 
 const authenticateUser = (req, res, prox) => {
 	const authToken = req.headers.authorization;
@@ -18,4 +20,44 @@ const authenticateUser = (req, res, prox) => {
 	}
 };
 
-module.exports = { authenticateUser };
+const authenticateUserAdmin = async (req, res, prox) => {
+	const authToken = req.headers.authorization;
+
+	if (!authToken) {
+		return res.status(401).send();
+	}
+	const token = authToken.split(" ")[1];
+
+	try {
+		verify(token, process.env.JWT_TOKEN);
+		await validateProfile(profileEnum.DASH_ADMIN, token);
+
+		return prox();
+	} catch (err) {
+		return res.status(401).send();
+	}
+};
+
+const authenticateUserDashboard = async (req, res, prox) => {
+	const authToken = req.headers.authorization;
+
+	if (!authToken) {
+		return res.status(401).send();
+	}
+	const token = authToken.split(" ")[1];
+
+	try {
+		verify(token, process.env.JWT_TOKEN);
+		await validateProfile(profileEnum.DASH_DEFAULT, token);
+
+		return prox();
+	} catch (err) {
+		return res.status(401).send();
+	}
+};
+
+module.exports = {
+	authenticateUser,
+	authenticateUserAdmin,
+	authenticateUserDashboard,
+};
